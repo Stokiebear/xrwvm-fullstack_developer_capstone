ddba0a546b43d1a298acbabf97c0a147a013772b
Stokiebear <travisws96@gmail.com>
2026-01-14 14:45:12 -0500

    Changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -42,13 +42,14 @@ REST_FRAMEWORK = {
 # Application definition
 
 INSTALLED_APPS = [
-    'djangoapp.apps.DjangoappConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoapp.apps.DjangoappConfig',
 ]
 
 MIDDLEWARE = [
